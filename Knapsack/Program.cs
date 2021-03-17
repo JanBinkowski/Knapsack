529e5982e3dc4089ee48301a866239cbb3ac4c5d
Jan Binkowski <jan.binkowski@wp.pl>
2021-03-17 14:24:24 +0100

    Nowy algorytm

--- Knapsack/Program.cs
+++ Knapsack/Program.cs
@@ -66,7 +66,7 @@ namespace Knapsack
         }
     }
     */
-
+    /*
     public class Algorithm
     {        
         private static int getMaxIndex(int[] weight, int[] values)
@@ -99,7 +99,29 @@ namespace Knapsack
             return value;
         }
     }
+    */
+    public class Algorithm
+    {
+        public int KnapSack(int capacity_is, int[] weights, int[] values, int amountOfItems)
+        {
+            int[,] K = new int[amountOfItems + 1, capacity_is + 1];
 
+            for (int i = 0; i <= amountOfItems; ++i)
+            {
+                for (int w = 0; w <= capacity_is; ++w)
+                {
+                    if (i == 0 || w == 0)
+                        K[i, w] = 0;
+                    else if (weights[i - 1] <= w)
+                        K[i, w] = Math.Max(values[i - 1] + K[i - 1, w - weights[i - 1]], K[i - 1, w]);
+                    else
+                        K[i, w] = K[i - 1, w];
+                }
+            }
+
+            return K[amountOfItems, capacity_is];
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -120,7 +142,7 @@ namespace Knapsack
 
             Algorithm algo = new();
             
-          Console.Write( "Solution: "+algo.getOptimalValue(capacity_is, weights,values));
+          Console.WriteLine( "Solution: " + algo.KnapSack(capacity_is, weights,values, amountOfItems));
           
             /*
             foreach (var item in weights)
